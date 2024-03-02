7e97ff203b4d61850dbd5b47a7c17ff63789b438
Nishitha Singh N <nishithasingh777@gmail.com>
2024-03-03 02:13:52 +0530

    second

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/screens/game_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,13 +16,13 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF323D5B),
+      backgroundColor: const Color(0xFF323D5B),
       body: Form(
         key: _formKey,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               "Enter Players Name",
               style: TextStyle(
                 fontSize: 30,
@@ -29,13 +30,93 @@ class _HomeScreenState extends State<HomeScreen> {
                 color: Colors.white,
               ),
             ),
-            SizedBox(height: 20),
-            Padding(padding: EdgeInsets.all(15),
+            const SizedBox(height: 20),
+            Padding(padding: const EdgeInsets.all(15),
             child: TextFormField(
               controller: player1Controller,
-              
+              style: const TextStyle(color: Colors.white),
+              decoration: const InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white)
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red)
+                ),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)
+                ),
+                errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red)
+                ),
+                hintText: "Player 1 Name",
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+              validator: (value){
+                if(value == null || value.isEmpty){
+                  return "Please enter player 1 name";
+                }
+                return null;
+              },
             ),
             ),
+            Padding(padding: const EdgeInsets.all(15),
+              child: TextFormField(
+                controller: player2Controller,
+                style: const TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white)
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white)
+                  ),
+                  errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red)
+                  ),
+                  hintText: "Player 2 Name",
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return "Please enter player 2 name";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            InkWell(
+              onTap: (){
+                if(_formKey.currentState!.validate()){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) =>GameScreen(
+                        player1: player1Controller.text,
+                        player2: player2Controller.text,
+                    ),
+                    )
+                  );
+                }
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 17,horizontal: 20),
+                child: Text(
+                  "Start Game",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                ),
+              )
+            )
           ],
         ),
       ),
